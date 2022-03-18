5ef6f91ab045820c2d531c7d83075bf870d7aed8
zakreed <zakreed7@gmail.com>
2022-03-18 12:51:55 +0000

    added files

---
+++ src/views/Homepage.vue
@@ -0,0 +1,41 @@
+<template>
+<nav class="flex justify-between m-3">
+    <h1 class="text-xl">Lavory</h1>
+    <h1 class="text-xl">About</h1>
+</nav>
+<div class="flex w-full justify-center">
+    <div class="w-full max-w-screen-lg mx-8">
+        <div class="flex justify-between my-10">
+            <input type="text" class="border">
+            <p>category</p>
+        </div>
+    
+        <div class="grid grid-cols-4 items-center gap-8 mb-10">
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+            <div class="w-full h-full bg-black text-white">
+                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            </div>
+        </div>
+    </div>
+</div>
+</template>
