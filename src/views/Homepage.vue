4abda0023edb91d6b91ee9c6ee838c5e4f5e5fc4
zakreed <zakreed7@gmail.com>
2022-03-19 13:00:36 +0000

    created recipe cards

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="flex justify-between m-3">
-    <h1 class="text-xl">Lavory</h1>
-    <h1 class="text-xl">About</h1>
+    <h1 class="text-2xl">Lavory</h1>
+    <h1 class="text-lg">About</h1>
 </nav>
 <div class="flex w-full justify-center">
     <div class="w-full max-w-screen-lg mx-8">
@@ -11,30 +11,86 @@
         </div>
     
         <div class="grid grid-cols-4 items-center gap-8 mb-10">
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+            
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
-            <div class="w-full h-full bg-black text-white">
-                <img src="src/assets/spaghetti.jpg" alt="spaghetti bolognesse">
+
+            <div class="w-full h-full bg-white rounded-lg">
+                <img class="max-h-96 rounded-t-lg" src="src/assets/steak.jpg" alt="spaghetti bolognesse">
+                <p class="text-xs px-4 pt-4 pb-2 text-gray-500">DINNER</p>
+                <h2 class="font-semibold px-4 pb-4">Spaghetti bolognesse</h2>
+                <p class="text-xs px-4 pb-4">A very nice meal that you can make for cheap in a very short amount of time</p>
+                <div class="flex justify-center">
+                    <button class="transition-colors border-green-500 border-2 text-green-500 hover:text-white hover:bg-green-500 active:bg-green-700 active:border-green-700 px-20 py-1 rounded-lg text-sm m-4">VIEW</button>
+                </div>
             </div>
+
         </div>
     </div>
 </div>
