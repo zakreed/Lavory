855d4cc8489e3a25e3b04c3075339c33d15ee955
zakreed <zakreed7@gmail.com>
2022-03-19 23:52:09 +0000

    added colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#ECF0F1',
+        'primary': '#26A69A',
+        'secondary': '#00796B',
+      }
+    },
   },
   plugins: [],
 }
